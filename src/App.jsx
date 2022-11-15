c60f3c103fdb4d4a1c84f5a8801c3626fce19e4f
daniel-monsalve-villegas <unbigote@hush.com>
2022-11-15 08:02:34 -0500

    feat: bookappointment component with textarea input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css';
-import urgent from './assets/urgentcare.png';
-import primary from './assets/primarycare.png';
-import Button from './components/Buttons/Button';
+import BookAppointment from './components/BookAppointment/BookAppointment';
 
 const App = () => (
   <div className="App">
-    <Button image={urgent} text="Urgent care" value="urgent care" />
-    <Button image={primary} text="Primary care" value="primary care" />
+    <BookAppointment />
   </div>
 );
 
