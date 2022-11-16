4fb0d757d41c1669a125646eb5a1ea4ce1b0a392
juan_lorza <lorza112@hotmail.com>
2022-11-15 20:13:02 -0500

    component Emergency

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Article from './components/Article/Article';
+import Emergency from './components/Emergency/Emergency';
+import Footer from './components/Footer/Footer';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <Article />
+      <Emergency />
+      <Footer />
     </header>
   </div>
 );
