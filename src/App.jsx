24a96c935b14d16b6ae91545a53dff2557845dcb
Baldorofaesir <salomoneyd@gmail.com>
2022-11-15 22:21:47 -0500

    Fixing Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-
 import './App.css';
-import Article from './components/Article/Article';
+import { useState } from 'react';
+import Header from './components/Header/Header';
+import WebPageInfoComponent from './components/WebPageInfoComponent/WebPageInfoComponent';
 import Emergency from './components/Emergency/Emergency';
+import BookAppointment from './components/BookAppointment/BookAppointment';
+import Article from './components/Article/Article';
 import Footer from './components/Footer/Footer';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <Article />
+const App = () => {
+  const [open, setOPen] = useState(false);
+
+  const toggle = () => {
+    setOPen(!open);
+  };
+  return (
+    <div className="App">
+      <header>
+        <Header toggle={toggle} open={open} />
+      </header>
       <Emergency />
+      <Article />
+      <BookAppointment />
+      <section>
+        <WebPageInfoComponent />
+      </section>
       <Footer />
-    </header>
-  </div>
-);
+    </div>
+  );
+};
 
 export default App;
