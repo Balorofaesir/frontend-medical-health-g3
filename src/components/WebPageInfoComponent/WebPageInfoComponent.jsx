ae77172e08cd86c11d9d39a4f3175e0c2fc11952
Baldorofaesir <salomoneyd@gmail.com>
2022-11-15 19:50:54 -0500

    fixing changes with buitrago

--- src/components/WebPageInfoComponent/WebPageInfoComponent.jsx
+++ src/components/WebPageInfoComponent/WebPageInfoComponent.jsx
@@ -1,33 +1,35 @@
-/* eslint-disable global-require */
-
 import './style2.css';
+import star from './assets/Star.jpeg';
+import handStar from './assets/hand-star.jpeg';
+import hospitalBed from './assets/hospital-bed.jpeg';
+import lab from './assets/lab.jpeg';
 
 const WebPageInfoComponent = () => (
   <section className="comp2FatherCont">
     <div className="comp2Cont">
       <div className="imgCont">
-        <img src={require('./assets/Star.jpeg')} alt="" />
+        <img src={star} alt="" />
       </div>
       <h1>20 +</h1>
       <p>years of experience</p>
     </div>
     <div className="comp2Cont">
       <div className="imgCont">
-        <img src={require('./assets/hand-star.jpeg')} alt="" />
+        <img src={handStar} alt="" />
       </div>
       <h1>99%</h1>
       <p>patients satisfied</p>
     </div>
     <div className="comp2Cont">
       <div className="imgCont">
-        <img src={require('./assets/hospital-bed.jpeg')} alt="" />
+        <img src={hospitalBed} alt="" />
       </div>
       <h1> 700+ </h1>
       <p>medical beads</p>
     </div>
     <div className="comp2Cont">
       <div className="imgCont">
-        <img src={require('./assets/lab.jpeg')} alt="" />
+        <img src={lab} alt="" />
       </div>
       <h1>50 +</h1>
       <p>laboratory experts</p>
