1c452d8e816afa10b4d6c393fbe267075fbee00b
Salomón Vásquez Castillo <112648027+Balorofaesir@users.noreply.github.com>
2022-11-15 22:12:13 -0500

    Hdm01 53 (#11)
    * Config code style
    
    * component Emergecy
    
    * component Emergency
    
    * component Emergency
    
    * Mdh01 52 (#8)
    
    * Update README.md
    
    * header component
    
    * correct use of folders
    
    * WebInfoComponent added
    
    * fixing changes with buitrago
    
    Co-authored-by: Juan M. Lorza <115027137+lorza12@users.noreply.github.com>
    
    * Hdm01 51 (#7)
    
    * component Emergecy
    
    * component Emergency
    
    * component Emergency
    
    Co-authored-by: Baldorofaesir <salomoneyd@gmail.com>
    
    * Mdh01 48 (#5)
    
    * feat:BookAppointmen component
    
    * feat: bookappointment component with date input
    
    * feat: bookappointment component with textarea input
    
    * feat: bookappointment component with textarea input
    
    * feat: better styles
    
    * feat: better styles
    
    * bookappointment component
    
    Co-authored-by: Baldorofaesir <salomoneyd@gmail.com>
    
    * Mdh01 49 (#3)
    
    * feat:Button component
    
    * feat: image from buttons added
    
    * button component
    
    * component Article
    
    * deleting body black background color
    
    * component footer
    
    * fixing folder name problem
    
    Co-authored-by: juan_lorza <lorza112@hotmail.com>
    Co-authored-by: Juan M. Lorza <115027137+lorza12@users.noreply.github.com>
    Co-authored-by: daniel monsalve <unbigote@hush.com>

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import { useState } from 'react';
+import Header from './components/Header/Header';
+import WebPageInfoComponent from './components/WebPageInfoComponent/WebPageInfoComponent';
+import Emergency from './components/Emergency/Emergency';
+import BookAppointment from './components/BookAppointment/BookAppointment';
+import Article from './components/CompArticle/Article';
+
+const App = () => {
+  const [open, setOPen] = useState(false);
+
+  const toggle = () => {
+    setOPen(!open);
+  };
+  return (
+    <div className="App">
+      <header>
+        <Header toggle={toggle} open={open} />
+      </header>
+      <Emergency />
+      <Article />
+      <BookAppointment />
+      <section>
+        <WebPageInfoComponent />
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Emergency/Emergency.jsx
@@ -0,0 +1,39 @@
+import './Emergency.css';
+
+import imgPhon from './assets/icono-de-la-línea-directa-del-teléfono-en-círculo-128428651.jpg';
+
+const Emergency = () => (
+
+  <div className="cont1">
+    <div className="cont1__section1">
+      <div className="cont1__img">
+        <img src={imgPhon} alt="imagenPhone" />
+      </div>
+      <div className="cont1_text1">
+        <h1> Emergency 24 hours</h1>
+        <h3>+04 8544 3322</h3>
+      </div>
+    </div>
+    <div className="cont1__section2">
+      <div className="cont1__text2">
+        <div className="cont1__list1">
+          <h1>Mon - Fri</h1>
+          <h2>08:00-08:00</h2>
+        </div>
+        <hr />
+        <div className="cont1__list2">
+          <h1>Sarutday</h1>
+          <h2>09:00-06:00</h2>
+        </div>
+        <hr />
+        <div className="cont1__list3">
+          <h1>Sanday</h1>
+          <h2>09:00-03:00</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+);
+
+export default Emergency;
