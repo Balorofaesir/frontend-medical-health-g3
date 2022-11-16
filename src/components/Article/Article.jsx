8d4cfaf639cc87fe1da28ef72c6bcb9d232d599e
juan_lorza <lorza112@hotmail.com>
2022-11-15 21:39:19 -0500

    component Article

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,31 @@
+import './Articles.css';
+
+import image from './assets/portrait-beautiful-mature-woman-doctor-260nw-1680655153.webp';
+
+const Article = () => (
+
+  <div className="cont">
+    <div className="cont__text">
+      <h1> Who we are</h1>
+      <h2> We provide the highest leve of care</h2>
+      <p>Nulla porttitoe accumsan tincidunt. Curabitur aliquet Curabitur
+        aliquet quam id dui posure blandit vivamus magna justo laciani eget consectetur
+        convallis at tellus proin eget torotr.
+      </p>
+    </div>
+
+    <div className="cont__imgText">
+      <img className="image" src={image} alt="doct" />
+      <p>Donec velit neque auctor sit amet aliquam vel ullamcorper sit amet ligula. Lorem
+        ipsum dolor sit amet consectetur adipising elit. Vivamus magna juso lacini aget consectetur
+        sed convallis at tellus.
+      </p>
+    </div>
+    <div className="cont__button">
+      <button type="button">About as </button>
+    </div>
+  </div>
+
+);
+
+export default Article;
