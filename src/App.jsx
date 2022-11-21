8a8d080a270b760114b695e7c0f063caddba5aed
Baldorofaesir <salomoneyd@gmail.com>
2022-11-21 14:51:42 -0500

    Adding Find a Dr

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Footer from './components/Footer/Footer';
 import HomePage from './pages/Home';
 import CartPage from './pages/Cart';
 import NotFound from './pages/NotFound404';
+import DoctorAndres from './pages/DoctorAndres/DoctorAndres';
+import DoctorMiguel from './pages/DoctorMiguel/DoctorMiguel';
+import DoctorJuan from './pages/DoctorJuan/DoctorJuan';
+import FindDr from './pages/FindDoctor/FindADoctor';
 
 const App = () => {
   const [open, setOPen] = useState(false);
@@ -21,6 +25,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="DoctorAndres" element={<DoctorAndres />} />
+        <Route path="DoctorMiguel" element={<DoctorMiguel />} />
+        <Route path="DoctorJuan" element={<DoctorJuan />} />
+        <Route path="FindDr" element={<FindDr />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
