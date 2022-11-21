18d0b4867f3250a99e1e2a1b8c53d1ca56968776
Salomón Vásquez Castillo <112648027+Balorofaesir@users.noreply.github.com>
2022-11-21 14:54:50 -0500

    Adding Find a Dr (#18)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import Types from 'prop-types';
-import './Header.css';
+import './header.css';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BsTelephonePlus, BsTelephone } from 'react-icons/bs';
 import { BiWorld } from 'react-icons/bi';
