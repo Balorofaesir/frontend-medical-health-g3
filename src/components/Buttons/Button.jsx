dd8c4502bedf2d8d47796a60d0b4ce11846b0378
daniel-monsalve-villegas <unbigote@hush.com>
2022-11-22 15:33:49 -0500

    buttons using Link from react-router-dom

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,33 +1,27 @@
-import Type from 'prop-types';
+import { Link } from 'react-router-dom';
 import urgent from './assets/urgentcare.png';
 import primary from './assets/primarycare.png';
 import './Button.css';
 
-const Button = ({ handleClick }) => (
+const Button = () => (
   <div className="container">
-    <a
-      href="../BookAppointment/BookAppointment.jsx"
-      className="btn__container"
-      value="urgent"
-      onClick={handleClick}
-    >
-      <img src={urgent} alt="button to use service" className="btn__img" />
-    </a>
-    <p className="btn__paragraph">Urgent care</p>
-    <a
-      href="../BookAppointment/BookAppointment.jsx"
-      className="btn__container"
-      value="urgent"
-      onClick={handleClick}
-    >
-      <img src={primary} alt="button to use service" className="btn__img" />
-    </a>
-    <p className="btn__paragraph">Primary care</p>
+    <div className="btn__container">
+      <Link to="/emergency" className="btn__container">
+        <img src={urgent} alt="boton to get urgent care" className="btn__img" />
+      </Link>
+      <p className="btn__paragraph">Urgent care</p>
+    </div>
+    <div className="btn__container">
+      <Link to="/primary" className="btn__container">
+        <img
+          src={primary}
+          alt="boton to get urgent care"
+          className="btn__img"
+        />
+      </Link>
+      <p className="btn__paragraph">Primary care</p>
+    </div>
   </div>
 );
-
-Button.propTypes = {
-  handleClick: Type.func.isRequired,
-};
 
 export default Button;
