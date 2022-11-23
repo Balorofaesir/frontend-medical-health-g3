aa9ed9da3d90a2d022299c0e0e55af710106b462
daniel-monsalve-villegas <unbigote@hush.com>
2022-11-22 19:25:17 -0500

    app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
 import './App.css';
-import { useState } from 'react';
-import Header from './components/Header/Header';
-import WebPageInfoComponent from './components/WebPageInfoComponent/WebPageInfoComponent';
-import Emergency from './components/Emergency/Emergency';
-import BookAppointment from './components/BookAppointment/BookAppointment';
-import Article from './components/Article/Article';
-import Footer from './components/Footer/Footer';
+import Button from './components/Buttons/Button';
 
-const App = () => {
-  const [open, setOPen] = useState(false);
-
-  const toggle = () => {
-    setOPen(!open);
-  };
-  return (
-    <div className="App">
-      <header>
-        <Header toggle={toggle} open={open} />
-      </header>
-      <Emergency />
-      <Article />
-      <BookAppointment />
-      <section>
-        <WebPageInfoComponent />
-      </section>
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <Button />
+  </div>
+);
 
 export default App;
