04f5f1776f9e1838e80e63c6c867f8e4eb0f73cb
Baldorofaesir <salomoneyd@gmail.com>
2022-11-23 14:11:22 -0500

    fixing app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ import Footer from './components/Footer/Footer';
 import HomePage from './pages/Home';
 import CartPage from './pages/Cart';
 import NotFound from './pages/NotFound404';
-import DoctorAndres from './pages/DoctorAndres/DoctorAndres';
-import DoctorMiguel from './pages/DoctorMiguel/DoctorMiguel';
-import DoctorJuan from './pages/DoctorJuan/DoctorJuan';
+import DoctorProfile from './pages/DoctorProfile/DoctorProfile';
 import FindDr from './pages/FindDoctor/FindADoctor';
 
 const App = () => {
   const [open, setOPen] = useState(false);
-
   const toggle = () => {
     setOPen(!open);
   };
@@ -25,9 +22,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="DoctorAndres" element={<DoctorAndres />} />
-        <Route path="DoctorMiguel" element={<DoctorMiguel />} />
-        <Route path="DoctorJuan" element={<DoctorJuan />} />
+        <Route path="DoctorProfile" element={<DoctorProfile />} />
         <Route path="FindDr" element={<FindDr />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
@@ -35,5 +30,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
