6e613bb7774c6240116dbb5b62aad17fc9dbc193
daniel-monsalve-villegas <unbigote@hush.com>
2022-11-22 19:08:59 -0500

    signup form redirect to profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import "./App.css";
-import Login from "./components/Login/Login";
+import { useNavigate } from 'react-router-dom';
+import './App.css';
 
 const App = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <Login />
+      <button type="submit" value="signup" onClick={() => navigate('/signup')}>
+        Sign up
+      </button>
+      <button type="submit" value="login" onClick={() => navigate('/login')}>
+        Login
+      </button>
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
