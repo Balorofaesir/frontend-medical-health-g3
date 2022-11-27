414f8359a429f070820c99f3047473172dac2884
Salomón Vásquez Castillo <112648027+Balorofaesir@users.noreply.github.com>
2022-11-27 15:16:28 -0500

    adding calendar page and modifiend namepage to match the url (#28)

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import './calendar.css';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+class MyCalendar extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, 'days')
+          .toDate(),
+        title: 'Some title',
+      },
+      // {
+      //   start: moment().toDate(),
+      //   end: moment()
+      //     .add(1, 'days')
+      //     .toDate(),
+      //   title: 'Second Date Test',
+      // },
+    ],
+  };
+
+  render() {
+    const { events } = this.state;
+    return (
+      <div className="Calendar">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={events}
+          style={{ height: '100vh' }}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyCalendar;
--- src/components/NamePages/NamePages.jsx
+++ src/components/NamePages/NamePages.jsx
@@ -1,8 +1,13 @@
 import './NamePages.css';
 
-const NamePages = () => (
-  <div className="NamePages">
-    <p>cart</p>
-  </div>
-);
+const NamePages = () => {
+  const url = window.location.pathname;
+  const name = url.slice(1);
+  return (
+    <div className="NamePages">
+      <p className="NamePages__url"> {name} </p>
+    </div>
+  );
+};
+
 export default NamePages;
